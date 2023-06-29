26c8b719adbe0e8a53f424dd3012f913fa044693
Ana <almendras451@gmail.com>
2023-06-29 15:17:13 -0400

    pasa de activity a otro

--- app/src/main/java/com/example/ejercicio12/MainActivity.java
+++ app/src/main/java/com/example/ejercicio12/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.ejercicio12;
 
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.ejercicio12.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public class MainActivity extends AppCompatActivity {
+        private ActivityMainBinding binding;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            binding = ActivityMainBinding.inflate(getLayoutInflater());
+            setContentView(binding.getRoot());
+
+
+            binding.button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent( MainActivity.this, MenuActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
+
+
     }
-}
